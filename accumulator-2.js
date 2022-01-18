92bf7232af5276c5f225156aa64114deb67d596f
username <nomadicalli@mail.ru>
2022-01-18 11:56:28 +0300

    Сложил все положительные значение в переменную

--- accumulator-2.js
+++ accumulator-2.js
@@ -1,2 +1,11 @@
 const num = [34, 123, 656, -2, 23, -54, 23, 645, -46, 23];
 
+let accumalator = 0;
+
+for (let i = 0; i < num.length; i++) {
+    if (num[i] > 0) {
+        accumalator += num[i];
+   }
+}
+
+console.log(accumalator)
