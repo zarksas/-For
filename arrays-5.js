e28e3846be5ef635d95ae0a3a67f71270a47ec19
username <nomadicalli@mail.ru>
2022-01-18 11:42:33 +0300

    Прописал цикл с условием

--- arrays-5.js
+++ arrays-5.js
@@ -1,1 +1,8 @@
 const names = ['Muskhab', 'Alvi', 'Ibrahim', 'Muhammad', 'Umar', 'Shamil', 'Islam', 'Adam', 'Turpal', 'Tamirlan'];
+
+for (let i = 0; i < names.length; i++) {
+    
+    if (names[i][0] == 'a'.toUpperCase()) {
+        console.log(names[i]);
+    }
+}
