3a586668e549eb94d54292b1f31c6c149cef8cf5
username <nomadicalli@mail.ru>
2022-01-18 11:47:27 +0300

    Прописал цикл с условием вывода четныч и нечетных чисел

--- arrays-6.js
+++ arrays-6.js
@@ -1,2 +1,13 @@
-const num = [22, 44, 53, 23, 5434, 232, 23, 646, 75, 1];
+const num = [29, 44, 53, 23, 5434, 232, 23, 646, 75, 1];
 
+for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 === 0) {
+        console.log(num[i])
+    }
+}
+
+for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 === 1) {
+        console.log(num[i])
+    }
+}
