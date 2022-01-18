1d101a1cad94190be943fcfddba4da73604f9440
username <nomadicalli@mail.ru>
2022-01-18 11:18:44 +0300

    написал цикл для массива

--- arrays-2.js
+++ arrays-2.js
@@ -1,1 +1,5 @@
 const arr = ['contra', 'hitman', 'callOfduty', 'GTA5', 'battlefield', 'medalOfhonor', 'manhunt', 'stronghold', 'strongholdCrusader', 'unreal'];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
