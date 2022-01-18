6c1d641c49ac7324ccd75e96dc35023b967d3f23
username <nomadicalli@mail.ru>
2022-01-18 12:14:58 +0300

    новый файл в массив accumulatorOdd добавил из массива num все нечетные числа

---
+++ accumulator-6.js
@@ -0,0 +1,9 @@
+const num = [12, 23, 464, 23, 3, 5, 6, 7, 10, 20];
+
+let accumulatorEven = [];
+    for (let i = 0; i < num.length; i++) {
+        if (num[i] % 2 === 1) {
+            accumulatorEven.push(num[i])
+        }
+    }
+console.log(accumulatorEven);
