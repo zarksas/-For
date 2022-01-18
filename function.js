6333fc4d3fca13ca303884f793f1f44fd20bea04
username <nomadicalli@mail.ru>
2022-01-18 14:13:03 +0300

    Написал функцию чтобы она возвращала новый массив из которого удалены все объекты с ключами deleted равным true

--- function.js
+++ function.js
@@ -35,3 +35,24 @@ const getSymbol = (lang) => {
     }
     return resNames
  }
+
+
+ function deleteMessages(array) {
+    let arr = []
+      for (let i = 0; i < array.length; i++) {
+          if (array[i].deleted !== true) {
+              arr.push(array[i])
+              
+          }
+      }
+    return arr;
+  }
+  
+  const messages = [
+   { text: 'hello', deleted: true },
+   { text: 'bue', deleted: false },
+   { text: 'js is a beauty', deleted: false },
+   { text: 'pair programming', deleted: true },
+   { text: 'this is my horse', deleted: false },
+  ];
+  console.log(deleteMessages(messages))
