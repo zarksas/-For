6aac722c5fda44bc43566031674d572a4fe0d37e
username <nomadicalli@mail.ru>
2022-01-18 11:51:27 +0300

    Сложил сумму массива в переменной

--- accumulator-1.js
+++ accumulator-1.js
@@ -1,1 +1,10 @@
 const number = [424, 12, 24, 67, 343, 12, 46, 5, 23, 22];
+
+let accumalator = 0;
+
+for (let i = 0; i < number.length; i++) {
+     accumalator += number[i]
+     
+}
+
+console.log(accumalator)
