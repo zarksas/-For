fa11ffa23a9e27720366f5e3b3dff310762cfaa8
username <nomadicalli@mail.ru>
2022-01-18 11:30:14 +0300

    Прописал к массиву цикл с условием

--- arrays-4.js
+++ arrays-4.js
@@ -1,2 +1,7 @@
 const arr =  ['JavaScript', 'Python', 'PHP', 'Java', 'C++', 'Go', 'C#', 'Fortan', 'Scratch', 'C'];
 
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length >= 3) {
+        console.log(arr[i])
+    }
+};
